f695ad20d3eebc27fb4b56a3f5b8eee5614b93d2
saikham <saikham.email@gmail.com>
2018-05-15 12:44:08 +0630

    Service Wroker - Caching Finished

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -9,7 +9,10 @@ if('serviceWorker' in navigator){
         .register('/sw.js')
         .then(function () {
             console.log('Service Worker registered.')
-        });
+        })
+        .catch(function(err){
+            console.log(err)
+        })
 }
 
 window.addEventListener('beforeinstallprompt', function (event) {
@@ -18,63 +21,3 @@ window.addEventListener('beforeinstallprompt', function (event) {
     deferredPrompt = event
 })
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET','http://httpbin.org/ip');
-xhr.responseType = 'json';
-
-xhr.onload = function () {
-    console.log(xhr.response)
-}
-
-xhr.onerror = function () {
-    console.log('Error!')
-}
-
-xhr.send();
-
-
-fetch('http://httpbin.org/ip')
-    .then(function (response) {
-        console.log(response)
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data)
-    })
-    .catch(function(err){
-        console.log(err)
-    });
-
-fetch('http://httpbin.org/post',{
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    mode: 'cors',
-    body: JSON.stringify({message: 'hello world'})
-    })
-    .then(function (response) {
-        console.log(response)
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data)
-    })
-    .catch(function(err){
-        console.log(err)
-    });
-
-// var promise = new Promise(function(resolve, reject){
-//    setTimeout(function () {
-//        resolve('now it is 3s')
-//    },3000)
-// })
-//
-// promise.then(function (text) {
-//     return text
-// }).then(function(nextText){
-//     console.log(nextText)
-// })
-//
-// console.log('next tasks')
